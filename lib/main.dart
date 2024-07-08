a0acf6ab336ebefb830eeca096ceacf7f1ecef42
lcros-mo <luiscros98@gmail.com>
2024-07-08 18:21:01 +0200

    debbuged and implemented FireBase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'colors.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'login_screen.dart';
 import 'home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
