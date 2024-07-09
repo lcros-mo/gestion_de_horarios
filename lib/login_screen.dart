3d9d25676073b72fb4fb19b35714948665f73d88
lcros-mo <luiscros98@gmail.com>
2024-07-09 14:08:39 +0200

    GoogleAuth WIP

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,62 +10,55 @@ class LoginScreen extends StatefulWidget {
 }
 
 class LoginScreenState extends State<LoginScreen> {
-  final _formKey = GlobalKey<FormState>();
-  String _username = '';
-  String _password = '';
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
-  void _login() {
-    if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
-      if (_username == 'admin' && _password == 'admin') {
+  Future<User?> _signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+    if (googleUser != null) {
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      final UserCredential userCredential = await _auth.signInWithCredential(credential);
+      return userCredential.user;
+    }
+    return null;
+  }
+
+  Future<void> _login() async {
+    User? user = await _signInWithGoogle();
+    if (user != null) {
+      bool isUserAllowed = await _isUserAllowed(user.email!);
+      if (isUserAllowed) {
         Navigator.pushReplacementNamed(context, '/home');
       } else {
         ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          content: Text('Invalid credentials'),
+          content: Text('Access denied'),
         ));
+        await _auth.signOut();
       }
     }
+  }
+
+  Future<bool> _isUserAllowed(String email) async {
+    // Aquí deberías implementar la lógica para verificar si el usuario pertenece a tu Google Admin
+    // Por ejemplo, podrías hacer una llamada a tu backend o verificar una lista de correos permitidos.
+    List<String> allowedEmails = ['user1@example.com', 'user2@example.com'];
+    return allowedEmails.contains(email);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Benvingut!')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Usuari',
-                    border: OutlineInputBorder(),
-                  ),
-                  onSaved: (value) => _username = value!,
-                  validator: (value) => value!.isEmpty ? 'Introdueix Usuari' : null,
-                ),
-                const SizedBox(height: 16),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Contrasenya',
-                    border: OutlineInputBorder(),
-                  ),
-                  obscureText: true,
-                  onSaved: (value) => _password = value!,
-                  validator: (value) => value!.isEmpty ? 'Introdueix Contrasenya' : null,
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: _login,
-                  child: const Text('Acceptar'),
-                ),
-              ],
-          ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _login,
+          child: const Text('Sign in with Google'),
         ),
       ),
     );
   }
 }
-
