0f7d2d098e0dfb26387e8821dab7dc905e90d723
lcros-mo <luiscros98@gmail.com>
2024-07-11 11:49:35 +0200

    undo revert

---
+++ android/app/src/main/kotlin/com/example/gestionDeHorarios/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gestionDeHorarios
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:googleapis/admin/directory_v1.dart' as admin;
+import 'package:googleapis_auth/auth_io.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,43 +15,59 @@ class LoginScreen extends StatefulWidget {
 }
 
 class LoginScreenState extends State<LoginScreen> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final auth.FirebaseAuth _auth = auth.FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email', 'profile', 'openid']);
 
-  Future<User?> _signInWithGoogle() async {
-    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-    if (googleUser != null) {
-      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-      final AuthCredential credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
-      );
-      final UserCredential userCredential = await _auth.signInWithCredential(credential);
-      return userCredential.user;
+  Future<auth.User?> _signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser != null) {
+        final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+        final auth.AuthCredential credential = auth.GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+        final auth.UserCredential userCredential = await _auth.signInWithCredential(credential);
+        return userCredential.user;
+      }
+    } catch (e) {
+      print(e.toString());
     }
     return null;
   }
 
   Future<void> _login() async {
-    User? user = await _signInWithGoogle();
+    auth.User? user = await _signInWithGoogle();
     if (user != null) {
+      print('User signed in: ${user.email}');
       bool isUserAllowed = await _isUserAllowed(user.email!);
       if (isUserAllowed) {
+        print('User is allowed');
         Navigator.pushReplacementNamed(context, '/home');
       } else {
+        print('User is not allowed');
         ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
           content: Text('Access denied'),
         ));
         await _auth.signOut();
       }
+    } else {
+      print('User sign in failed');
     }
   }
 
   Future<bool> _isUserAllowed(String email) async {
-    // Aquí deberías implementar la lógica para verificar si el usuario pertenece a tu Google Admin
-    // Por ejemplo, podrías hacer una llamada a tu backend o verificar una lista de correos permitidos.
-    List<String> allowedEmails = ['user1@example.com', 'user2@example.com'];
-    return allowedEmails.contains(email);
+    final String serviceAccountJson = await rootBundle.loadString('assets/service_account.json');
+    final Map<String, dynamic> serviceAccount = json.decode(serviceAccountJson);
+    final credentials = ServiceAccountCredentials.fromJson(serviceAccount);
+    final authClient = await clientViaServiceAccount(credentials, [
+      admin.DirectoryApi.adminDirectoryUserReadonlyScope
+    ]);
+
+    final directory = admin.DirectoryApi(authClient);
+    final admin.Users users = await directory.users.list(customer: 'my_customer', query: 'email=$email');
+
+    return users.users != null && users.users!.isNotEmpty;
   }
 
   @override
